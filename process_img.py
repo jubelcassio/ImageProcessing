f6d956b01d2dcd2bcd7cb286c9dac650c83828a4
JuBzzz <jubz147@gmail.com>
2018-07-04 22:29:02 -0300

    Added argparse.
    Easier to parse the arguments and add support for optional arguments.

--- process_img.py
+++ process_img.py
@@ -1,33 +1,31 @@
 import sys
 import os
 from PIL import Image
+import argparse
 
-supported_formats = ["BMP","EPS","GIF","ICO","JPG","JPEG","PNG","TIFF","WEBP"]
-
+supported_formats = ["bmp","eps","gif","ico","jpg","jpeg","png","tiff","webp"]
+actions = ['convert']
 
 def convert(path, filetype):
     if os.path.isfile(path):
         o_type = path[-3:]
-        if o_type.upper() == filetype.upper():
+        if o_type == filetype:
             print("{} is already of type {}".format(path, filetype))
-        elif o_type.upper() in supported_formats:
+        elif o_type in supported_formats:
             new_image_path = "{}.converted.{}".format(path[:-4], filetype)
             im = Image.open(path)
             im.save(new_image_path)
             print("{} saved successfully.".format(new_image_path))
-        else:
-            msg = "{} is not a supported format.\nSupported formats: {}"
-            print(msg.format(o_type, supported_formats))
 
-    if os.path.isdir(path):
+    elif os.path.isdir(path):
         file_list = os.listdir(path)
 
         for file_ in file_list:
             f_path = os.path.join(path, file_)
             f_type = file_[-3:]
-            if f_type.upper() == filetype.upper():
+            if f_type == filetype:
                 print("{} is already of type {}".format(f_path, filetype))
-            elif f_type.upper() in supported_formats:
+            elif f_type in supported_formats:
                 new_image_path = "{}.converted.{}".format(f_path[:-4], filetype)
                 im = Image.open(f_path)
                 im.save(new_image_path)
@@ -40,15 +38,13 @@ def convert(path, filetype):
 
 
 if __name__ == '__main__':
-    action = sys.argv[1]
+    parser = argparse.ArgumentParser()
 
-    if action == "convert":
-        if len(sys.argv) < 3:
-            print("Missing argument, try:\nprocess_img convert [directory/file] [image type]")
-        elif len(sys.argv) < 4:
-            img_type = sys.argv[2]
-            convert(".", img_type)
-        else:
-            img_file = sys.argv[2]
-            img_type = sys.argv[3]
-            convert(img_file, img_type)
+    parser.add_argument('action', type=str, choices=actions)
+    parser.add_argument('path', type=str)
+    parser.add_argument('filetype', type=str, choices=supported_formats)
+
+    args = parser.parse_args(sys.argv[1:])
+
+    if args.action == "convert":
+            convert(args.path, args.filetype)
