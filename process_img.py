a73950c39d339cb32fa485684f9dcb0edca7feb2
JuBzzz <jubz147@gmail.com>
2018-07-07 13:02:59 -0300

    Splited the functions into a call_[function] and the actua function.
    Now the call_[function] will run the input parsing and validation and call the actual function on the given images.

--- process_img.py
+++ process_img.py
@@ -6,7 +6,17 @@ import argparse
 supported_formats = ["bmp","eps","gif","ico","jpg","jpeg","png","tiff","webp"]
 actions = ['convert', 'resize']
 
-def convert(pos_args):
+def convert(path, filetype):
+    if path[-3:] == filetype:
+        print("{} is already of type {}".format(path, filetype))
+    else:
+        new_image_path = "{}.converted.{}".format(path[:-4], filetype)
+        im = Image.open(path)
+        im.save(new_image_path)
+        print("{} saved successfully.".format(new_image_path))
+
+def call_convert(pos_args):
+    ## Parse the inputs
     parser = argparse.ArgumentParser(prog="convert")
 
     parser.add_argument('path', type=str)
@@ -16,37 +26,44 @@ def convert(pos_args):
 
     path, filetype = args.path, args.filetype
 
-    if os.path.isfile(path):
-        o_type = path[-3:]
-        if o_type not in supported_formats:
-            print("{} does not have a supported file type.".format(path))
-        elif o_type == filetype:
-            print("{} is already of type {}".format(path, filetype))
-        else:
-            new_image_path = "{}.converted.{}".format(path[:-4], filetype)
-            im = Image.open(path)
-            im.save(new_image_path)
-            print("{} saved successfully.".format(new_image_path))
 
+    ## Convert single file
+    if os.path.isfile(path):
+        if path[-3:] not in supported_formats:
+            print("{} does not have a supported file type.".format(path))
+        else:
+            convert(path, filetype)
+
+
+    ## Convert files in directory
     elif os.path.isdir(path):
         file_list = os.listdir(path)
-
         for file_ in file_list:
             f_path = os.path.join(path, file_)
             f_type = file_[-3:]
-            if f_type == filetype:
-                print("{} is already of type {}".format(f_path, filetype))
-            elif f_type in supported_formats:
-                new_image_path = "{}.converted.{}".format(f_path[:-4], filetype)
-                im = Image.open(f_path)
-                im.save(new_image_path)
-                print("{} saved successfully.".format(new_image_path))
+            if f_type in supported_formats:
+                convert(f_path, filetype)
+
 
     else:
         msg = "{} is not a valid file path or directory."
         print(msg.format(path))
 
-def resize(pos_args):
+
+def resize(path, size):
+    new_image_path = "{}.resized.{}".format(path[:-4], path[-3:])
+    im = Image.open(path)
+
+    if im.size == size:
+        print("{} is already of size {}".format(path, size))
+    else:
+        resized_im = im.resize(size)
+        resized_im.save(new_image_path)
+        print("{} saved successfully.".format(new_image_path))
+
+
+def call_resize(pos_args):
+    ## Parse the inputs
     parser = argparse.ArgumentParser(prog="resize")
 
     parser.add_argument('path', type=str)
@@ -55,22 +72,18 @@ def resize(pos_args):
 
     args = parser.parse_args(pos_args)
 
-    path, width, height = args.path, args.width, args.height
+    path, size = args.path, (args.width, args.height)
 
+
+    ## Resize single file
     if os.path.isfile(path):
-        o_type = path[-3:]
-        if o_type not in supported_formats:
+        if path[-3:] not in supported_formats:
             print("{} does not have a supported file type.".format(path))
         else:
-            new_image_path = "{}.resized.{}".format(path[:-4], o_type)
-            im = Image.open(path)
-            if im.size == (width, height):
-                print("{} is already of size {}".format(path, (width, height)))
-            else:
-                resized_im = im.resize((width, height))
-                resized_im.save(new_image_path)
-                print("{} saved successfully.".format(new_image_path))
+            resize(path, size)
 
+
+    ## Resize files in directory
     elif os.path.isdir(path):
         file_list = os.listdir(path)
 
@@ -78,14 +91,7 @@ def resize(pos_args):
             f_path = os.path.join(path, file_)
             f_type = file_[-3:]
             if f_type in supported_formats:
-                new_image_path = "{}.resized.{}".format(f_path[:-4], f_type)
-                im = Image.open(f_path)
-                if im.size == (width, height):
-                    print("{} is already of size {}".format(f_path, (width, height)))
-                else:
-                    resized_im = im.resize((width, height))
-                    resized_im.save(new_image_path)
-                    print("{} saved successfully.".format(new_image_path))
+                resize(f_path, size)
 
     else:
         msg = "{} is not a valid file path or directory."
@@ -99,8 +105,8 @@ if __name__ == '__main__':
     action = sys.argv[1]
 
     if action == "convert":
-        convert(sys.argv[2:])
+        call_convert(sys.argv[2:])
     if action == "resize":
-        resize(sys.argv[2:])
+        call_resize(sys.argv[2:])
     else:
         print("Invalid action: '{}', choose from: {}".format(action, actions))
