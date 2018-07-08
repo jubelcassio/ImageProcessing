aa49473c66e2b5451b14ff6fd984bba7ad07cda2
JuBzzz <jubz147@gmail.com>
2018-07-08 14:27:09 -0300

    Fixed import bug.
    Imported all the script files on the __init__.py of the actions submodule.

--- actions/__init__.py
+++ actions/__init__.py
@@ -1,2 +1,4 @@
-
 supported_formats = ["bmp","eps","gif","ico","jpg","jpeg","png","tiff","webp"]
+__all__ = ['convert', 'resize', 'fit', 'scale']
+
+from . import *
--- process_img.py
+++ process_img.py
@@ -8,22 +8,23 @@ import os
 from PIL import Image, ImageColor
 import argparse
 import re
-import actions
+from actions import *
 
-supported_formats = ["bmp","eps","gif","ico","jpg","jpeg","png","tiff","webp"]
+
 action_list = ['convert', 'resize', 'scale', 'fit']
+
 
 if __name__ == '__main__':
     action = sys.argv[1]
 
     if action == "convert":
-        actions.convert.call_convert(sys.argv[2:])
+        convert.call_convert(sys.argv[2:])
     elif action == "resize":
-        actions.resize.call_resize(sys.argv[2:])
+        resize.call_resize(sys.argv[2:])
     elif action == "scale":
-        actions.scale.call_scale(sys.argv[2:])
+        scale.call_scale(sys.argv[2:])
     elif action == "fit":
-        actions.fit.call_fit(sys.argv[2:])
+        fit.call_fit(sys.argv[2:])
     else:
         msg = "Invalid action: '{}', choose from: {}".format(action,
                                                              action_list)
