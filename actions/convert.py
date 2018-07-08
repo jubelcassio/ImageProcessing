7e6982ec371810eef4908254db4fe87d0e03f44a
JuBzzz <jubz147@gmail.com>
2018-07-08 16:16:53 -0300

    Separated the parsing from call_convert function into its own function.

--- actions/convert.py
+++ actions/convert.py
@@ -4,7 +4,9 @@ from PIL import Image
 from actions import supported_formats
 
 def convert(path, filetype):
-    if path[-3:] == filetype:
+    if path[-3:] not in supported_formats:
+        print("{} does not have a supported file type.".format(path))
+    elif path[-3:] == filetype:
         print("{} is already of type {}".format(path, filetype))
     else:
         new_image_path = "{}.converted.{}".format(path[:-4], filetype)
@@ -12,36 +14,31 @@ def convert(path, filetype):
         im.save(new_image_path)
         print("{} saved successfully.".format(new_image_path))
 
-
-def call_convert(pos_args):
+def parse(user_args):
     ## Parse the inputs
     parser = argparse.ArgumentParser(prog="convert")
 
     parser.add_argument('path', type=str)
     parser.add_argument('filetype', type=str, choices=supported_formats)
 
-    args = parser.parse_args(pos_args)
+    args = parser.parse_args(user_args)
+    return (args.path, args.filetype)
 
-    path, filetype = args.path, args.filetype
 
+def call_convert(user_args):
+    path, filetype = parse(user_args)
 
     ## Convert single file
     if os.path.isfile(path):
-        if path[-3:] not in supported_formats:
-            print("{} does not have a supported file type.".format(path))
-        else:
-            convert(path, filetype)
-
+        convert(path, filetype)
 
     ## Convert files in directory
     elif os.path.isdir(path):
         file_list = os.listdir(path)
         for file_ in file_list:
             f_path = os.path.join(path, file_)
-            f_type = file_[-3:]
-            if f_type in supported_formats:
+            if os.path.isfile(f_path):
                 convert(f_path, filetype)
-
 
     else:
         msg = "{} is not a valid file path or directory."
