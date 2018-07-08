aa0f7e98497ff01d3eefa9b5b582ade392f6d613
JuBzzz <jubz147@gmail.com>
2018-07-08 16:25:13 -0300

    Separated the argument parsing from the call_resize function onto its own function.

--- actions/resize.py
+++ actions/resize.py
@@ -4,6 +4,10 @@ from PIL import Image
 from actions import supported_formats
 
 def resize(path, size):
+    if path[-3:] not in supported_formats:
+        print("{} does not have a supported file type.".format(path))
+        return
+
     new_image_path = "{}.resized.{}".format(path[:-4], path[-3:])
     im = Image.open(path)
 
@@ -14,8 +18,7 @@ def resize(path, size):
         resized_im.save(new_image_path)
         print("{} saved successfully.".format(new_image_path))
 
-
-def call_resize(pos_args):
+def parse(user_args):
     ## Parse the inputs
     parser = argparse.ArgumentParser(prog="resize")
 
@@ -23,17 +26,17 @@ def call_resize(pos_args):
     parser.add_argument('width', type=int)
     parser.add_argument('height', type=int)
 
-    args = parser.parse_args(pos_args)
+    args = parser.parse_args(user_args)
 
-    path, size = args.path, (args.width, args.height)
+    return args.path, (args.width, args.height)
 
+
+def call_resize(user_args):
+    path, size = parse(user_args)
 
     ## Resize single file
     if os.path.isfile(path):
-        if path[-3:] not in supported_formats:
-            print("{} does not have a supported file type.".format(path))
-        else:
-            resize(path, size)
+        resize(path, size)
 
 
     ## Resize files in directory
@@ -42,8 +45,7 @@ def call_resize(pos_args):
 
         for file_ in file_list:
             f_path = os.path.join(path, file_)
-            f_type = file_[-3:]
-            if f_type in supported_formats:
+            if os.path.isfile(f_path):
                 resize(f_path, size)
 
     else:
