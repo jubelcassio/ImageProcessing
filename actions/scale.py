a20199b97e8a631e8e4e5e9240c43d2794e87c52
JuBzzz <jubz147@gmail.com>
2018-07-09 16:23:51 -0300

    Added ability to change the saving format while using 'scale' on images.

--- actions/scale.py
+++ actions/scale.py
@@ -2,15 +2,24 @@ import argparse
 import os
 from PIL import Image
 from actions import supported_formats
+from actions import supported_modes
 
 
-def run(path, scalar):
+def run(path, scalar, save_as):
     if path[-3:] not in supported_formats:
         print("{} does not have a supported file type.".format(path))
         return
 
-    new_image_path = "{}.scaled.{}".format(path[:-4], path[-3:])
     im = Image.open(path)
+
+    if save_as == None:
+        save_as = path[-3:]
+    if im.mode not in supported_modes[save_as]:
+        new_mode = supported_modes[save_as][-1]
+        im = im.convert(new_mode)
+        print("Converting {} to {} image...".format(im.mode, new_mode))
+
+    new_image_path = "{}.scaled.{}".format(path[:-4], save_as)
 
     # Minimum width/height is 1 pixel.
     size = (max([1, round(im.size[0] * scalar)]),
@@ -26,5 +35,7 @@ def parse(user_args):
     parser = argparse.ArgumentParser(prog="scale")
 
     parser.add_argument('scalar', type=float)
+    parser.add_argument('--save_as', type=str, choices=supported_formats,
+                        default=None)
 
     return vars(parser.parse_args(user_args))
