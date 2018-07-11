0eb31d6ff8e4c5c2b49fc2fe9353c1f32835d91b
JuBzzz <jubz147@gmail.com>
2018-07-11 18:43:44 -0300

    Reestructured the scale module.
    Refactored the 'scale.py' file to allow a more modular design, the objective is so each script can be easily tested and expanded.

--- actions/scale.py
+++ actions/scale.py
@@ -4,34 +4,51 @@ from PIL import Image
 from actions import supported_formats
 from actions import supported_modes
 
-
-def run(path, scalar, save_as):
+def open_image(path):
     extension = os.path.splitext(path)[1][1:]
     if extension not in supported_formats:
         print("{} does not have a supported file type.".format(path))
         return
 
-    im = Image.open(path)
+    try:
+        im = Image.open(path)
+    except OSError:
+        print("{} cannot be identified as an image file.".format(path))
+    return im
 
-    if save_as == None:
-        save_as = extension
-
-    # Change the image mode, if needed
-    if im.mode not in supported_modes[save_as]:
-        new_mode = supported_modes[save_as][-1]
-        im = im.convert(new_mode)
-        print("Converting {} to {} image...".format(im.mode, new_mode))
-
-    # New name, so the original image isn't overwritten
-    new_image_path = "{}.scaled.{}".format(path[:-4], save_as)
-
+def scale(im, scalar):
     # Minimum width/height is 1 pixel.
     size = (max([1, round(im.size[0] * scalar)]),
             max([1, round(im.size[1] * scalar)]))
 
-    resized_im = im.resize(size)
-    resized_im.save(new_image_path)
+    return im.resize(size)
+
+def save_image(im, path, save_as):
+    name, extension = os.path.splitext(os.path.basename(path))
+    # Removing the 'dot' at the start
+    extension = extension[1:]
+
+    if save_as == None:
+        save_as = extension
+
+        # Change the image mode, if needed
+        if im.mode not in supported_modes[save_as]:
+            new_mode = supported_modes[save_as][-1]
+            im = im.convert(new_mode)
+            print("Converting {} to {} image...".format(im.mode, new_mode))
+
+    # New name, so the original image isn't overwritten
+    new_image_path = "{}.scaled.{}".format(name, save_as)
+
+    im.save(new_image_path)
     print("{} saved successfully.".format(new_image_path))
+
+
+def run(path, scalar, save_as):
+    im = open_image(path)
+    if im is not None:
+        resized_im = scale(im, scalar)
+        save_image(resized_im, path, save_as)
 
 
 def parse(user_args):
