5ce8b23122a32279dca468441e6cac30df081fc9
JuBzzz <jubz147@gmail.com>
2018-07-11 19:07:11 -0300

    Refactor of the convert module.
    This commit breaks the mode conversion of the module.

--- actions/convert.py
+++ actions/convert.py
@@ -4,33 +4,45 @@ from PIL import Image
 from actions import supported_formats
 from actions import supported_modes
 
-def run(path, filetype, mode):
+
+def open_image(path):
     extension = os.path.splitext(path)[1][1:]
     if extension not in supported_formats:
         print("{} does not have a supported file type.".format(path))
         return
 
+    try:
+        im = Image.open(path)
+    except OSError:
+        print("{} cannot be identified as an image file.".format(path))
+        return
+    return im
+
+def save_image(im, path, save_as):
+    name, extension = os.path.splitext(os.path.basename(path))
+    # Removing the 'dot' at the start
+    extension = extension[1:]
+
+    if save_as == None:
+        save_as = extension
+
+        # Change the image mode, if needed
+        if im.mode not in supported_modes[save_as]:
+            new_mode = supported_modes[save_as][-1]
+            im = im.convert(new_mode)
+            print("Converting {} to {} image...".format(im.mode, new_mode))
+
     # New name, so the original image isn't overwritten
-    new_image_path = "{}.converted.{}".format(path[:-4], filetype)
-
-    im = Image.open(path)
-
-    # Change the image mode, if needed:
-    if mode == None:
-        mode = im.mode
-
-    if mode not in supported_modes[filetype]:
-        msg = "{} is not supported by the {} image writer."
-        print(msg.format(mode, filetype))
-
-        mode = supported_modes[filetype][-1]
-
-    if mode != im.mode:
-        print("Converting {} to {} image...".format(im.mode, mode))
-        im = im.convert(mode)
+    new_image_path = "{}.converted.{}".format(name, save_as)
 
     im.save(new_image_path)
     print("{} saved successfully.".format(new_image_path))
+
+def run(path, filetype, mode):
+    im = open_image(path)
+    if im is not None:
+        save_image(im, path, filetype)
+
 
 def parse(user_args):
     ## Parse the inputs
